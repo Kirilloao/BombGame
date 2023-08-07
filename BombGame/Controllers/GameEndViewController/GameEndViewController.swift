d7c623e85ef62723d93f8687f98e870c6a75b742
Kirilloao <kirilltaraturin747@gmail>
2023-08-07 20:57:10 +0200

    setup color to mainView

--- BombGame/Controllers/GameEndViewController/GameEndViewController.swift
+++ BombGame/Controllers/GameEndViewController/GameEndViewController.swift
@@ -6,3 +6,34 @@
 //
 
 import Foundation
+import UIKit
+
+class GameEndViewController: UIViewController {
+    
+    private lazy var gradientView: GradientView = {
+      let gradientView = GradientView(frame: view.bounds)
+      gradientView.translatesAutoresizingMaskIntoConstraints = false
+      return gradientView
+    }()
+    
+    // MARK: - Life Cycle Methods
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        subviews()
+        setupConstraints()
+    }
+    
+    private func subviews() {
+      view.addSubview(gradientView)
+    }
+
+    func setupConstraints() {
+      NSLayoutConstraint.activate([
+        gradientView.topAnchor.constraint(equalTo: view.topAnchor),
+        gradientView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+        gradientView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+        gradientView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+      ])
+    }
+}
