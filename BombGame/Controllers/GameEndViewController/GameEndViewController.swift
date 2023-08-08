19967c069de9bde4ff4644ecde157859347ecb40
Kirilloao <kirilltaraturin747@gmail>
2023-08-08 12:53:17 +0200

    add image "bang" to assets

--- BombGame/Controllers/GameEndViewController/GameEndViewController.swift
+++ BombGame/Controllers/GameEndViewController/GameEndViewController.swift
@@ -11,22 +11,21 @@ import UIKit
 class GameEndViewController: UIViewController {
     
     // MARK: - UI properties
+    private var mainStackView = UIStackView()
+    
     private lazy var gradientView: GradientView = {
-      let gradientView = GradientView(frame: view.bounds)
-      gradientView.translatesAutoresizingMaskIntoConstraints = false
-      return gradientView
+        let gradientView = GradientView(frame: view.bounds)
+        gradientView.translatesAutoresizingMaskIntoConstraints = false
+        return gradientView
     }()
     
-    private lazy var firstLabel: UILabel = {
-        let label = UILabel()
-        label.text = "Проигравший выполянет задание"
-        label.textColor = .black
-        label.font = UIFont.boldSystemFont(ofSize: 30)
-        label.numberOfLines = 2
-        label.lineBreakMode = .byWordWrapping
-        label.textAlignment = .center
-
-        label.translatesAutoresizingMaskIntoConstraints = false
+    private lazy var taskLabel: UILabel = {
+        let label = createLabel(
+            with: "Проигравший выполянет задание",
+            textColor: .black,
+            font: UIFont.boldSystemFont(ofSize: 25),
+            lines: 2
+        )
         
         return label
     }()
@@ -34,132 +33,131 @@ class GameEndViewController: UIViewController {
     private lazy var bombImageView: UIImageView = {
         let imageView = UIImageView()
         imageView.image = UIImage(named: "bang")
+        imageView.contentMode = .scaleAspectFit
         
         return imageView
     }()
     
-    private lazy var secondLabel: UILabel = {
-        let secondLabel = UILabel()
-        secondLabel.text = "В следующем раунде после каждого ответа хлопать в ладоши"
-        secondLabel.textColor = .purpleLabel
-        secondLabel.font = UIFont.boldSystemFont(ofSize: 30)
-        secondLabel.numberOfLines = 0
-        secondLabel.lineBreakMode = .byWordWrapping
-        secondLabel.translatesAutoresizingMaskIntoConstraints = false
+    private lazy var descriptionTaskLabel: UILabel = {
+        let secondLabel = createLabel(
+            with: "В следующем раунде после каждого ответа хлопать в ладоши",
+            textColor: .purpleLabel,
+            font: UIFont.boldSystemFont(ofSize: 20),
+            lines: 0
+        )
         
         return secondLabel
     }()
     
-    private lazy var anotherTaskButton: UIButton = {
-//        let taskButton = UIButton(type: .system)
-//        taskButton.setTitle("Другое Задание", for: .normal)
-//        taskButton.backgroundColor = .purpleColor
-//        taskButton.tintColor = .yellowLabel
-//        taskButton.frame = CGRect(x: 0, y: 0, width: 274, height: 79)
-//        taskButton.translatesAutoresizingMaskIntoConstraints = false
-        let taskButton = createButton()
+    private lazy var anotherTaskButton: CustomButton = {
+        let taskButton = CustomButton(customTitle: "Другое Задание")
         
         return taskButton
     }()
     
-    private lazy var startOverButton: UIButton = {
-
-        let taskButton = createButton()
+    private lazy var startOverButton: CustomButton = {
+        let taskButton = CustomButton(customTitle: "Начать Заново")
         
         return taskButton
     }()
     
     
-    
-    private var mainStackView = UIStackView()
     
     // MARK: - Life Cycle Methods
     override func viewDidLoad() {
         super.viewDidLoad()
         
-//        addView(gradientView,
-//                firstLabel,
-//                bombImageView,
-//                secondLabel,
-//                anotherTaskButton,
-//                mainStackView
-//        )
-        addView(gradientView,
-            mainStackView)
-   
+        addSubviews()
+        
         setupConstraints()
         setupNavigationBar()
         createStackView()
-        
-        
     }
     
     // MARK: - Private Methods
-    
-    private func addView(_ view: UIView...) {
-        view.forEach { subview in
-            self.view.addSubview(subview)
-        }
+    private func addSubviews() {
+        view.addSubview(gradientView)
+        view.addSubview(mainStackView)
     }
-
+    
     private func setupConstraints() {
         
-      // setup constraints to gradientView
-      NSLayoutConstraint.activate([
-        gradientView.topAnchor.constraint(equalTo: view.topAnchor),
-        gradientView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
-        gradientView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
-        gradientView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
-      ])
+        // setup constraints to gradientView
+        NSLayoutConstraint.activate([
+            gradientView.topAnchor.constraint(equalTo: view.topAnchor),
+            gradientView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            gradientView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            gradientView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+        ])
+        
+        // setup constraints to firstLabel
+        NSLayoutConstraint.activate([
+            taskLabel.heightAnchor.constraint(equalToConstant: 80)
+        ])
+        
+        // setup constraints to secondLabel
+        NSLayoutConstraint.activate([
+            descriptionTaskLabel.heightAnchor.constraint(equalToConstant: 50)
+        ])
     }
     
     private func createStackView() {
+        
         mainStackView.axis = .vertical
         mainStackView.spacing = 20
         mainStackView.alignment = .center
         mainStackView.distribution = .fill
         mainStackView.translatesAutoresizingMaskIntoConstraints = false
-        mainStackView.backgroundColor = .red
         
         NSLayoutConstraint.activate([
-            mainStackView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
-            mainStackView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
-            mainStackView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16),
-            mainStackView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+            mainStackView.topAnchor.constraint(
+                equalTo: view.safeAreaLayoutGuide.topAnchor
+            ),
+            mainStackView.leadingAnchor.constraint(
+                equalTo: view.leadingAnchor,
+                constant: 16
+            ),
+            mainStackView.trailingAnchor.constraint(
+                equalTo: view.trailingAnchor,
+                constant: -16
+            ),
+            mainStackView.bottomAnchor.constraint(
+                equalTo: view.bottomAnchor,
+                constant: -30
+            ),
         ])
         
-        
-        mainStackView.addArrangedSubview(firstLabel)
+        mainStackView.addArrangedSubview(taskLabel)
         mainStackView.addArrangedSubview(bombImageView)
-        mainStackView.addArrangedSubview(secondLabel)
+        mainStackView.addArrangedSubview(descriptionTaskLabel)
         mainStackView.addArrangedSubview(anotherTaskButton)
         mainStackView.addArrangedSubview(startOverButton)
     }
     
-    private func createButton() -> UIButton {
-        let taskButton = UIButton(type: .system)
-        taskButton.setTitle("Другое Задание", for: .normal)
-        taskButton.backgroundColor = .purpleLabel
-        taskButton.tintColor = .yellow
-        taskButton.frame = CGRect(x: 0, y: 0, width: 274, height: 79)
-        taskButton.translatesAutoresizingMaskIntoConstraints = false
+    private func createLabel(with text: String, textColor: UIColor, font: UIFont, lines: Int) -> UILabel {
         
-        return taskButton
+        let label = UILabel()
+        label.text = text
+        label.textColor = textColor
+        label.font = font
+        label.numberOfLines = lines
+        label.lineBreakMode = .byWordWrapping
+        label.textAlignment = .center
+        
+        label.translatesAutoresizingMaskIntoConstraints = false
+        
+        return label
     }
-    
-
-    
-    
-
 }
 
 // MARK: - NavigationBar
 extension GameEndViewController {
     private func setupNavigationBar() {
         let navBarAppearance = UINavigationBarAppearance()
-        navBarAppearance.largeTitleTextAttributes = [.font: UIFont.systemFont(ofSize: 30)]
         
-        navBarAppearance.titleTextAttributes = [.foregroundColor: UIColor.purpleLabel]
+        navBarAppearance.titleTextAttributes = [
+            .foregroundColor: UIColor.purpleLabel
+        ]
         navigationController?.navigationBar.standardAppearance = navBarAppearance
         
         title = "Игра"
