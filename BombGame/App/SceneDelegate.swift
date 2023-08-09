cec789fd202fdd18e84d76ccfa39eadfea3fb075
Loveink <106765014+Loveink@users.noreply.github.com>
2023-08-09 11:14:29 +0300

    fix navBar

--- BombGame/App/SceneDelegate.swift
+++ BombGame/App/SceneDelegate.swift
@@ -14,11 +14,26 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
 
+        let rootViewController = GameEndViewController()
+        let navigationController = UINavigationController(rootViewController: rootViewController)
+
+        let backButton = UIBarButtonItem(image: UIImage(systemName: "chevron.left"), style: .plain, target: self, action: #selector(goBack))
+        backButton.tintColor = .blackBackButton
+        rootViewController.navigationItem.leftBarButtonItem = backButton
+
+        navigationController.navigationBar.titleTextAttributes = [
+          NSAttributedString.Key.foregroundColor: UIColor.purpleColor,
+            NSAttributedString.Key.font: UIFont.boldSystemFont(ofSize: 30)
+        ]
+
         window = UIWindow(windowScene: windowScene)
-        let mainVC = MainViewController()
-        let navigationController = UINavigationController(rootViewController: mainVC)
         window?.rootViewController = navigationController
         window?.makeKeyAndVisible()
+    }
 
+    @objc private func goBack() {
+        if let navigationController = window?.rootViewController as? UINavigationController {
+            navigationController.popViewController(animated: true)
+        }
     }
 }
